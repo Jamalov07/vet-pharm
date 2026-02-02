7c90c1aa5cfa35bf7f9d2badeff2f813aba1eb55
No'monjon Jamalov <n.jamalov@tengebank.uz>
2026-02-02 15:51:09 +0500

    o

--- src/modules/client/client.repository.ts
+++ src/modules/client/client.repository.ts
@@ -247,7 +247,7 @@ export class ClientRepository implements OnModuleInit {
 			where: {
 				type: { in: ['client', 'selling'] },
 				deletedAt: null,
-				total: { not: null, gt: 0 },
+				total: { gt: 0 },
 				...(query.startDate && { createdAt: { gte: query.startDate } }),
 				...(query.endDate && { createdAt: { lte: query.endDate } }),
 			},
